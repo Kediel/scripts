84e00e983aeebdbd5c6cc18cec94e6f4ee830d5c
k3m@s <km5655@nyu.edu>
2022-04-28 16:42:20 -0400

    Powershill script to ping IPs defined in excel.

---
+++ pingTest.ps1
@@ -0,0 +1,55 @@
+# Check for if the file exists in defined path.
+#Test-Path -Path "C:\Users\...\Box\SSC CNI Drive\MMSC\etc_hosts_analysis.xlsx" -PathType Leaf
+
+
+
+$filename = Get-childItem "C:\Users\...\Box\SSC CNI Drive\MMSC\etc_hosts_analysis.xlsx"
+$sheetName = "etc_hosts updates only"
+
+$objExcel = New-Object -ComObject Excel.Application
+$WorkBook = $objExcel.Workbooks.Open($filename)
+$sheet = $WorkBook.Worksheets.Item($sheetName)
+ $objExcel.visible = $false
+ $objExcel.DisplayAlerts = $false
+
+$rowMax = $sheet.usedRange.Rows.Count
+
+try{
+    
+    for ($i=2; $i -le $rowMax; $i++){
+    
+        $hosts = $objExcel.Columns.Item(1).Rows.Item($i).Text
+        Write-Host "Pinging: $hosts" # Prints output
+         
+        if ( Test-Connection $hosts -Count 2 -Delay 1 -Quiet ) {
+                
+                    Write-Host "$hosts - Success"
+            
+            } else {
+                
+                Write-Host "$hosts - Failed"
+            }
+     }
+} catch {
+
+    Write-Warning "Ping Failed: $hosts"
+    <#Write-Warning "Error message: $_"
+
+     if ( $errorlog ) {
+
+                $errormsg = $_.ToString()
+                $exception = $_.Exception
+                $stacktrace = $_.ScriptStackTrace
+                $failingline = $_.InvocationInfo.Line
+                $positionmsg = $_.InvocationInfo.PositionMessage
+                $pscommandpath = $_.InvocationInfo.PSCommandPath
+                $failinglinenumber = $_.InvocationInfo.ScriptLineNumber
+                $scriptname = $_.InvocationInfo.ScriptName
+
+                Write-Verbose "Start writing to Error log."
+                Write-ErrorLog -hostname $computer -env $env -logicalname $logicalname -errormsg $errormsg -exception $exception -scriptname $scriptname -failinglinenumber $failinglinenumber -failingline $failingline -pscommandpath $pscommandpath -positionmsg $pscommandpath -stacktrace $stacktrace
+                Write-Verbose "Finish writing to Error log."
+            } #>
+}
+
+$objExcel.quit()
