4182a296dc3b5bbabffe3f0d27a1af3df0abed19
k3m@s <km5655@nyu.edu>
2022-05-05 12:14:35 -0400

    Update netconf_test.py
    Code runs, now to test on real environment.

--- netconf_test.py
+++ netconf_test.py
@@ -3,11 +3,13 @@
 """
 Created on Wed May  4 13:36:15 2022
 https://www.cisco.com/c/en/us/support/docs/storage-networking/management/200933-YANG-NETCONF-Configuration-Validation.html
-*remember to enable NETCONF/YANG on the asset by typing netconf-yang in a conf t
+https://stackoverflow.com/questions/50305725/condahttperror-http-000-connection-failed-for-url-https-repo-continuum-io-pk
 
+*remember to enable NETCONF/YANG on the asset by typing netconf-yang in a conf t
+*Also need the ncclient library installed: pip install ncclient
 """
 import sys, os, re, base64, socket
-from ncclient import *
+from ncclient import manager
     
 # Get device run config
 def test(host, user, pw):
@@ -40,9 +42,9 @@ def test(host, user, pw):
 if __name__ == "__main__":
     
     # Get user input
-    host = input("What is the host?")
-    user = input("What is the username?")
-    pw = input("What is the password?")
+    host = input("What is the host? ")
+    user = input("What is the username? ")
+    pw = input("What is the password? ")
     
     # Call the function
     test(host,user,pw)
